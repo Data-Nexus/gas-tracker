4b4f12ce1c4c1b104e438c349ce2476498c7d24e
Data-Nexus <derek.m.meyer@gmail.com>
2021-12-26 22:55:41 -0500

    Cleaned up some of the auto-gen comments

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,8 +3,7 @@ import { ERC20, Transfer, Burn } from "../generated/ERC20/ERC20"
 import { Block } from "../generated/schema"
 
 export function handleTransfer(event: Transfer): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
+  
   let entity = Block.load(event.block.number.toString())
 
   if (!entity) {
@@ -13,8 +12,9 @@ export function handleTransfer(event: Transfer): void {
     entity.id = event.block.number.toString()
     entity.BaseFee = event.block.baseFeePerGas
     entity.BlockNum = event.block.number
-      // Entities can be written to the store with `.save()`
+
     entity.save()
   }
 
  }
+
