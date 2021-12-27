a4f05a77142f20a759510baa370a2a5fd2245dae
Data-Nexus <derek.m.meyer@gmail.com>
2021-12-26 22:44:45 -0500

    Initial Version

--- generated/schema.ts
+++ generated/schema.ts
@@ -11,31 +11,27 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
-export class ExampleEntity extends Entity {
+export class Block extends Entity {
   constructor(id: string) {
     super();
     this.set("id", Value.fromString(id));
-
-    this.set("count", Value.fromBigInt(BigInt.zero()));
-    this.set("from", Value.fromBytes(Bytes.empty()));
-    this.set("to", Value.fromBytes(Bytes.empty()));
   }
 
   save(): void {
     let id = this.get("id");
-    assert(id != null, "Cannot save ExampleEntity entity without an ID");
+    assert(id != null, "Cannot save Block entity without an ID");
     if (id) {
       assert(
         id.kind == ValueKind.STRING,
-        "Cannot save ExampleEntity entity with non-string ID. " +
+        "Cannot save Block entity with non-string ID. " +
           'Considering using .toHex() to convert the "id" to a string.'
       );
-      store.set("ExampleEntity", id.toString(), this);
+      store.set("Block", id.toString(), this);
     }
   }
 
-  static load(id: string): ExampleEntity | null {
-    return changetype<ExampleEntity | null>(store.get("ExampleEntity", id));
+  static load(id: string): Block | null {
+    return changetype<Block | null>(store.get("Block", id));
   }
 
   get id(): string {
@@ -47,30 +43,37 @@ export class ExampleEntity extends Entity {
     this.set("id", Value.fromString(value));
   }
 
-  get count(): BigInt {
-    let value = this.get("count");
-    return value!.toBigInt();
+  get BlockNum(): BigInt | null {
+    let value = this.get("BlockNum");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toBigInt();
+    }
   }
 
-  set count(value: BigInt) {
-    this.set("count", Value.fromBigInt(value));
+  set BlockNum(value: BigInt | null) {
+    if (!value) {
+      this.unset("BlockNum");
+    } else {
+      this.set("BlockNum", Value.fromBigInt(<BigInt>value));
+    }
   }
 
-  get from(): Bytes {
-    let value = this.get("from");
-    return value!.toBytes();
+  get BaseFee(): BigInt | null {
+    let value = this.get("BaseFee");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toBigInt();
+    }
   }
 
-  set from(value: Bytes) {
-    this.set("from", Value.fromBytes(value));
-  }
-
-  get to(): Bytes {
-    let value = this.get("to");
-    return value!.toBytes();
-  }
-
-  set to(value: Bytes) {
-    this.set("to", Value.fromBytes(value));
+  set BaseFee(value: BigInt | null) {
+    if (!value) {
+      this.unset("BaseFee");
+    } else {
+      this.set("BaseFee", Value.fromBigInt(<BigInt>value));
+    }
   }
 }
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,57 +1,20 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { ERC20, Transfer, Burn } from "../generated/ERC20/ERC20"
-import { ExampleEntity } from "../generated/schema"
+import { Block } from "../generated/schema"
 
 export function handleTransfer(event: Transfer): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = Block.load(event.block.number.toString())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new Block(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity.id = event.block.number.toString()
+    entity.BaseFee = event.block.baseFeePerGas
+    entity.BlockNum = event.block.number
+      // Entities can be written to the store with `.save()`
+    entity.save()
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.from = event.params.from
-  entity.to = event.params.to
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.name(...)
-  // - contract.approve(...)
-  // - contract.totalSupply(...)
-  // - contract.transferFrom(...)
-  // - contract.decimals(...)
-  // - contract.burn(...)
-  // - contract.balanceOf(...)
-  // - contract.burnFrom(...)
-  // - contract.symbol(...)
-  // - contract.approveAndCall(...)
-  // - contract.allowance(...)
-}
-
-export function handleBurn(event: Burn): void {}
+ }
